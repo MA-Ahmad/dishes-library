82788c4dd034efe774dae127de171a107c34506b
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-06-17 16:38:52 -0700

    unused components removed

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -8,7 +8,7 @@ import {
   Row
 } from "reactstrap";
 import { Link } from "react-router-dom";
-import { Control, Form, Errors, actions } from "react-redux-form";
+import { Control, Form, Errors } from "react-redux-form";
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || val.length <= len;
@@ -86,7 +86,7 @@ class Contact extends Component {
               >
                 <i className="fa fa-phone"></i> Call
               </a>
-              <a role="button" className="btn btn-info">
+              <a role="button" className="btn btn-info" href="#">
                 <i className="fa fa-skype"></i> Skype
               </a>
               <a
